ccb3a83a362b5870c385771ae208b44590c98efc
Omar Einea <eineao@gmail.com>
2018-03-26 00:45:53 +0400

    Added exception handling on all threads and limited their number

--- scraper.py
+++ scraper.py
@@ -1,11 +1,14 @@
+from threading import Thread, active_count
 from lxml.html import fromstring as parse_html
+from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
 from selenium.webdriver import Chrome
 from datetime import date, timedelta
-from threading import Thread
-import requests, os
+import requests, os, re
 
+whitespaces = re.compile("\s+")
 url = "http://www.alkhaleej.ae/Archive?date="
 path = "./articles/"
 scraped_days = []
@@ -16,6 +19,7 @@ else:
     scraped_days.extend(file[:-4] for file in os.listdir(path))
 
 day = date.today()
+threads = []
 br = Chrome()
 br.set_page_load_timeout(7)
 
@@ -23,12 +27,22 @@ br.set_page_load_timeout(7)
 def scrape_day_links(links, day):
     with open(path + day + ".txt", "w+", encoding="utf-8") as file:
         for link in links:
-            content = parse_html(requests.get(link).text).find(".//div[@id='MainContent']")
-            categories = [a.text for a in content.findall(".//div[@id='BreadCrumb']/div/a")[:2]]
-            title = content.find(".//div[@class='Details_MainTitle']").text.strip()
-            body = content.find(".//div[@id='detailedBody']").text_content().replace("\n", "")
-            file.write("\t".join([link[38:], *categories, title, body]) + "\n")
-            print("Added Article:", title)
+            try:
+                html = requests.get(link).text
+                content = parse_html(html).find(".//div[@id='MainContent']")
+                breadcrumbs = content.findall(".//div[@id='BreadCrumb']/div/a")
+                if len(breadcrumbs) == 0:
+                    breadcrumbs = content.findall(".//div[@class='ThebreadCrumbContainer']//a")
+                categories = [a.text.strip() for a in breadcrumbs[:2]]
+                title = content.find(".//div[@class='Details_MainTitle']").text.strip()
+                body = content.find(".//div[@id='detailedBody']")
+                if body is None:
+                    body = content.find(".//div[@class='DetailsArticleSummary']")
+                body = whitespaces.sub(" ", body.text_content()).strip()
+                file.write("\t".join([link[37:], *categories, title, body]) + "\n")
+                print("Added Article:", title)
+            except Exception as error:
+                print("In link", link, "Error", error)
 
 
 while day.year > 2007:
@@ -38,20 +52,25 @@ while day.year > 2007:
     try: br.get(url + day.strftime("%d/%m/%Y"))
     except TimeoutException: pass
     links = []
-    for block in br.find_elements_by_class_name("ArchivingBlock"):
-        for section in block.find_elements_by_class_name("SectionItem"):
-            section.click()
-            WebDriverWait(section, 7).until(lambda section: "Selected" in section.get_attribute("class").split())
-            pages = len(block.find_elements_by_class_name("PageNumber"))
-            for page in range(pages + 1):
-                links.extend(
-                    link.get_attribute("href")
-                    for link in block.find_elements_by_xpath(".//ul[@class='ArchivUL']/li/a")
-                )
-                if page != pages:
-                    block.find_element_by_xpath(".//span[contains(@class,'NextPage')]/a").click()
-                    WebDriverWait(block, 7).until(
-                        lambda block: block.find_element_by_class_name("activePage").text == str(page + 2)
+    try:
+        for block in br.find_elements_by_class_name("ArchivingBlock"):
+            for section in block.find_elements_by_class_name("SectionItem"):
+                section.click()
+                WebDriverWait(section, 7).until(lambda section: "Selected" in section.get_attribute("class").split())
+                pages = len(block.find_elements_by_class_name("PageNumber"))
+                for page in range(pages + 1):
+                    links.extend(
+                        link.get_attribute("href")
+                        for link in block.find_elements_by_xpath(".//ul[@class='ArchivUL']/li/a")
                     )
-    print("Found", len(links), "Articles in Day:", day_string)
-    Thread(target=scrape_day_links, args=(links, day_string)).start()
+                    if page != pages:
+                        block.find_element_by_xpath(".//span[contains(@class,'NextPage')]/a").click()
+                        WebDriverWait(block, 7).until(
+                            ec.text_to_be_present_in_element((By.CLASS_NAME, "activePage"), str(page + 2))
+                        )
+        print("Found", len(links), "Articles in Day:", day_string)
+        threads.append(Thread(target=scrape_day_links, args=(links, day_string)))
+        if active_count() < 7:
+            threads.pop(0).start()
+    except Exception as error:
+        print("Skipped day", day_string, "Because of Error", error)
