7587c021457ef6c86ee1abb41307ec45ebfad5f1
Omar Einea <eineao@gmail.com>
2018-03-30 12:46:07 +0300

    Added articles manipulation tools - Part 2 cleaning & splitting

--- tools.py
+++ tools.py
@@ -32,3 +32,44 @@ def get_articles_count():
     for _ in get_articles():
         count += 1
     return count
+
+
+def clean_articles():
+    ids = set()
+    valid = 0
+    invalid = 0
+    repeated = 0
+    with open("cleaned_" + articles, "w+", encoding="utf-8") as outfile:
+        for article in get_articles():
+            parts = article.split("\t")
+            if len(parts) != 6:
+                invalid += 1
+            elif parts[0] in ids:
+                repeated += 1
+            else:
+                valid += 1
+                ids.add(parts[0])
+                outfile.write(article)
+    print("Cleaned", invalid, "invalid and", repeated, "repeated articles")
+    print(f"Output {valid} articles to cleaned_{articles}")
+
+
+def split_articles(n):
+    articles_parts = "{}.".join(articles.split("."))
+    part_length = get_articles_count() // n
+    for index, article in enumerate(get_articles()):
+        if index % part_length == 0 and index / part_length < n:
+            outfile = open(articles_parts.format(1 + index // part_length), "w+", encoding="utf-8")
+        outfile.write(article)
+
+
+def split_articles_by_categories():
+    for article in get_articles():
+        id_, date, category, sub_category, text = article.split("\t", 4)
+        while True:
+            try: outfile = open(f"{category}/{sub_category}.txt", "a", encoding="utf-8")
+            except FileNotFoundError: os.mkdir(category)
+            except OSError: sub_category = sub_category.replace('"', '')
+            else: break
+        outfile.write("\t".join([id_, date, text]))
+        outfile.close()
